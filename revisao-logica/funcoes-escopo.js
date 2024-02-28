c53825bc7ff211c4b2cf212d5927adec3cfcd803
Igor-OliveiraDev <igorchavesdev@gmail.com>
2024-02-28 06:43:16 -0300

    Tratando sobre escopo
    Escopos internos e externos e a execução deles

--- revisao-logica/funcoes-escopo.js
+++ revisao-logica/funcoes-escopo.js
@@ -15,3 +15,17 @@ function mostraN(){
 mostraN()
 
 console.log("Valor de N no escopo global: " + n)
+
+function fnExt() {
+
+    let n = "n local na função fn externo"
+
+    function fnInt(){
+        let n = "N no local da função interna"
+        console.log(n)
+    }
+    fnInt()
+    console.log(n)
+}
+    
+    fnExt()
